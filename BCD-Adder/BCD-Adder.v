a58c8065a5a38c5498abcff07fcc0ad321a4c75b
SohanSikdar <134160489+Sohansik02@users.noreply.github.com>
2023-09-25 01:08:08 +0530

    Update BCD-Adder.v

--- BCD-Adder/BCD-Adder.v
+++ BCD-Adder/BCD-Adder.v
@@ -3,23 +3,23 @@ module bcd_adder(a,b,cin,sum,cout);
     input cin;
     output [3:0] sum;
     output cout;
-    reg [4:0] temp;
+    reg [4:0] sumtemp;
     reg [3:0] sum;
     reg cout;  
 
     always @(a,b,cin)
     begin
-        temp = a+b+cin; 
-        if(temp > 9)    
+        sumtemp = a+b+cin; 
+        if(sumtemp > 9)    
 	begin
-            temp = temp+6; //add 6, if result is more than 9.
+            sumtemp = sumtemp+6; //add 6, if result is more than 9.
             cout = 1;  //set the carry output
-            sum = temp[3:0];   
+            sum = sumtemp[3:0];   
    	end
         else    
 	begin
             cout = 0;
-            sum = temp[3:0];
+            sum = sumtemp[3:0];
         end
     end     
 
